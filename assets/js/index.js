52bdacc9f48d601b1c5d397173ea84c2903d7913
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-03-23 21:30:05 +0200

    function definition && function declaration example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
-const userInput = prompt("Enter the message: ");
-alert(userInput);
+//const userInput = prompt("Enter the message: ");
+//alert(userInput);
 
+const myFirstFunc = function(){
+  console.log("1");
+}
+
+function mySecondFunc(){
+  console.log("2");
+}
+
+myFirstFunc();
+mySecondFunc();
+
+//console.log(myFirstFunc);
+//console.log(mySecondFunc);
